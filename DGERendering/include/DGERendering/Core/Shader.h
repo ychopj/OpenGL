2c3ab850b978450df66f7352725e57c1210bcc4a
ychopj <y1012486375@163.com>
2024-04-13 13:47:45 +0800

    添加openglLearn

--- DGERendering/include/DGERendering/Core/Shader.h
+++ DGERendering/include/DGERendering/Core/Shader.h
@@ -1,22 +1,28 @@
 #pragma once
+
 #include <string>
 #include <glm/glm.hpp>
 #include <unordered_map>
-namespace DGERendering::Core
-{
-    class Shader
-    {
+
+namespace DGERendering::Core {
+    class Shader {
     private:
         unsigned int m_programId;
         std::unordered_map<std::string, unsigned int> m_uniforms;
-        unsigned int getCompliedShader(unsigned int p_shaderType, const std::string& p_shaderSource);
+
+        unsigned int getCompliedShader(unsigned int p_shaderType, const std::string &p_shaderSource);
+
+        void checkCompileErrors(unsigned int shader, std::string type);
 
     public:
         Shader() = default;
-        bool Load(const std::string& p_vertexShaderFile, const std::string& p_fragmentShaderFile);
 
-        void         Use();
-        void         Unload();
+        bool Load(const std::string &p_vertexShaderFile, const std::string &p_fragmentShaderFile);
+
+        void Use();
+
+        void Unload();
+
         unsigned int GetProgramId() { return m_programId; }
 
         void Set(const std::string &name, int value);
@@ -25,13 +31,13 @@ namespace DGERendering::Core
 
         void Set(const std::string &name, float value);
 
-        void Set(const std::string &name, const glm::vec2& value);
+        void Set(const std::string &name, const glm::vec2 &value);
 
-        void Set(const std::string &name, const glm::vec3& value);
+        void Set(const std::string &name, const glm::vec3 &value);
 
-        void Set(const std::string &name, const glm::vec4& value);
+        void Set(const std::string &name, const glm::vec4 &value);
 
-        void Set(const std::string &name, const glm::mat4& value);
+        void Set(const std::string &name, const glm::mat4 &value);
 
         unsigned int GetAttributeLocation(const std::string &p_name);
     };
